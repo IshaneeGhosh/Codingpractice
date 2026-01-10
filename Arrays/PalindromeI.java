00f97d348b4dd400895064d7018a06215a9cdae9
IshaneeGhosh <165460141+IshaneeGhosh@users.noreply.github.com>
2026-01-10 12:22:04 +0530

    Create PalindromeI.java

---
+++ Arrays/PalindromeI.java
@@ -0,0 +1,45 @@
+/*
+Given a string s, return true if it is a palindrome, otherwise return false.
+A palindrome is a string that reads the same forward and backward. 
+It is also case-insensitive and ignores all non-alphanumeric characters.
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+Example 1:
+Input: s = "Was it a car or a cat I saw?"
+Output: true
+Explanation: After considering only alphanumerical characters we have "wasitacaroracatisaw", which is a palindrome.
+
+Example 2:
+Input: s = "tab a cat"
+Output: false
+Explanation: "tabacat" is not a palindrome.
+
+Constraints:
+1 <= s.length <= 1000
+s is made up of only printable ASCII characters.
+*/
+class PalindromeI {
+    public boolean isPalindrome(String s) {
+        int left=0,
+            right=s.length()-1;
+        s=s.toLowerCase();
+        while(left<right)
+        {   if(!Character.isLetterOrDigit(s.charAt(left)))
+            {
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(right)))
+            {
+                right--;
+                continue;
+            }
+            if(s.charAt(left)!=s.charAt(right))
+                return false;
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
