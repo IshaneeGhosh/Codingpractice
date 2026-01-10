ad41e7661c3eabd1ce59fddd49b80422fb3b356c
IshaneeGhosh <165460141+IshaneeGhosh@users.noreply.github.com>
2026-01-10 12:18:09 +0530

    Create ValidPalindrome.java

---
+++ Arrays/ValidPalindrome.java
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/valid-palindrome-ii/
+
+Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+Example 1:
+Input: s = "aba"
+Output: true
+
+Example 2:
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+Example 3:
+Input: s = "abc"
+Output: false
+ 
+Constraints:
+1 <= s.length <= 105
+s consists of lowercase English letters.
+*/
+class ValidPalindrome {
+    public boolean validPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
